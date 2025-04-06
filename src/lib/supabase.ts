8bb2c6318f5f06a7644b3a78d96cbbc3cd1389dc
RandQuran <siddiqmutarji.m@gmail.com>
2025-04-06 05:36:15 +0300

    before anon

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,16 @@
+// src/lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL or Anon Key is missing.");
+}
+
 export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY,
+  supabaseUrl,
+  supabaseAnonKey,
   {
     auth: {
       flowType: "pkce",
